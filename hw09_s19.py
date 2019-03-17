53b251c024a66f8c03aade198abd10fc7c3343cf
Krista Gurney <gurneykri@gmail.com>
2019-03-16 20:30:37 -0600

    added stuff

--- hw09_s19.py
+++ hw09_s19.py
@@ -2,10 +2,12 @@
 
 ####################################
 # module: hw09_s19.py
-# YOUR NAME
-# YOUR A#
+# Krista Gurney
+# A01671888
 ####################################
 
+import csv
+import os
 
 def generate_file_names(ftype, rootdir):
     '''
@@ -22,86 +24,87 @@ def generate_file_names(ftype, rootdir):
             generate_file_names(ftype, d)
 
 def display_csv_file(csv_file_path):
-  fd = {}
-  with open(csv_file_path, 'r') as instream:
-    reader = csv.reader(instream, delimiter = ",")
-    for row in reader:
-      print(row)
+    fd = {}
+    with open(csv_file_path, 'r') as instream:
+        reader = csv.reader(instream, delimiter = ",")
+        for row in reader:
+            print(row)
 
 def read_csv_file(csv_file_path):
-  fd = {}
-  with open(csv_file_path, 'r') as instream:
-    reader = csv.reader(instream, delimiter = ",")
-    reader.next() #to skip the header
-    for row in reader:
-      secs, up, down, lat = int(row[0]), float(row[1]), \
-                            float(row[2]), float(row[3])
-      fd[secs] = (up, down, lat)
-    return fd
+    fd = {}
+    with open(csv_file_path, 'r') as instream:
+        reader = csv.reader(instream, delimiter = ",")
+        # reader.next() #to skip the header
+        line_count = 0
+        for row in reader:
+            if line_count > 0:
+                secs, up, down, lat = int(row[0]), float(row[1]), \
+                                    float(row[2]), float(row[3])
+                fd[secs] = (up, down, lat)
+            else:
+                line_count += 1
+        return fd
 
 def plot_bee_traffic(csv_fp):
-  ## your code here
-  pass
+    ## your code here
+    pass
 
 def bee_traffic_estimate(t, md='u', fd={}):
-  assert md == 'u' or md == 'd' or md == 'l'
-  vals = fd.get(int(t))
-  if vals is None:
-    return None
-  elif md == 'u':
-    return vals[0]
-  elif md == 'd':
-    return vals[1]
-  elif md == 'l':
-    return vals[2]
+    assert md == 'u' or md == 'd' or md == 'l'
+    vals = fd.get(int(t))
+    if vals is None:
+        return None
+    elif md == 'u':
+        return vals[0]
+    elif md == 'd':
+        return vals[1]
+    elif md == 'l':
+        return vals[2]
 
 def make_bee_traffic_estimator(fd, md):
-  assert md == 'u' or md == 'd' or md == 'l'
-  return lambda t: bee_traffic_estimate(t, md=md, fd=fd)
+    assert md == 'u' or md == 'd' or md == 'l'
+    return lambda t: bee_traffic_estimate(t, md=md, fd=fd)
 
 
 import numpy as np
 def bee_traffic_stats(fd):
-  ## your code here
-  pass
+    ## your code here
+    pass
 
 def find_smallest_up_down_gap_file(csv_dir):
-  ## your code here
-  pass
+    ## your code here
+    pass
 
 def find_largest_up_down_gap_file(csv_dir):
-  ## your code here
-  pass
+    ## your code here
+    pass
 
 ############################
 
 def find_max_up_file(csv_dir):
-  ## your code here
-  pass
+    ## your code here
+    pass
 
 def find_min_up_file(csv_dir):
-  ## your code here
-  pass
+    ## your code here
+    pass
 
 ###########################
 
 def find_max_down_file(csv_dir):
-  ## your code here
-  pass
+    ## your code here
+    pass
 
 def find_min_down_file(csv_dir):
-  ## your code here
-  pass
+    ## your code here
+    pass
 
 ############################
 
 def find_max_lat_file(csv_dir):
-  ## your code here
-  pass
+    ## your code here
+    pass
 
 def find_min_lat_file(csv_dir):
-  ## your code here
-  pass
-
-
-  
+    ## your code here
+    pass
--- test.py
+++ test.py
@@ -1,183 +1,27 @@
-from const import const
-from maker import make_const, make_pwr, make_pwr_expr, make_plus, make_prod, make_quot, make_e_expr, make_ln, make_absv
-from tof import tof
-from riemann import riemann_approx, riemann_approx_with_gt, plot_riemann_error
-from deriv import deriv
-from antideriv import antideriv, antiderivdef
-from defintegralapprox import midpoint_rule, trapezoidal_rule, simpson_rule
+# from const import const
+# from maker import make_const, make_pwr, make_pwr_expr, make_plus, make_prod, make_quot, make_e_expr, make_ln, make_absv
+# from tof import tof
+# from riemann import riemann_approx, riemann_approx_with_gt, plot_riemann_error
+# from deriv import deriv
+# from antideriv import antideriv, antiderivdef
+# from defintegralapprox import midpoint_rule, trapezoidal_rule, simpson_rule
+from hw09_s19 import display_csv_file, read_csv_file
 import unittest
 import math
 
 class Assign01UnitTests(unittest.TestCase):
 
-    def test_01(self):
-        #(3x^2 +e^x) with the midpoint riemann sum on a partition of 10 subintervals
-        print("****Unit Test 01********")
-        fex1 = make_prod(make_const(3.0), make_pwr('x', 2.0))
-        fex = make_plus(fex1, make_e_expr(make_pwr('x', 1.0)))
-        print(fex)
-        err_list = riemann_approx_with_gt(fex,
-                                          make_const(-1.0),
-                                          make_const(1.0),
-                                          make_const(4.35),
-                                          make_const(10),
-                                          pp=0)
-
-        for n, err in err_list:
-            print(n, err)
-        print("Unit Test 01: pass")
+    # def test_01(self):
+    #     print("****Unit Test 01********")
+    #     display_csv_file('bee_traffic_estimates\\192_168_4_5-2018-07-01_08-00-10.csv')
+    #     print("Unit Test 01: pass")
 
     def test_02(self):
-        #(3x^2 +e^x) with the left point riemann sum on a partition of 10 subintervals
         print("****Unit Test 02********")
-        fex1 = make_prod(make_const(3.0), make_pwr('x', 2.0))
-        fex = make_plus(fex1, make_e_expr(make_pwr('x', 1.0)))
-        print(fex)
-        err_list = riemann_approx_with_gt(fex,
-                                          make_const(-1.0),
-                                          make_const(1.0),
-                                          make_const(4.35),
-                                          make_const(10),
-                                          pp=-1)
-
-        for n, err in err_list:
-            print(n, err)
+        fd = read_csv_file('bee_traffic_estimates\\192_168_4_5-2018-07-01_08-00-10.csv')
+        print(fd[5])
+        print(fd[28])
         print("Unit Test 02: pass")
-
-    def test_03(self):
-        #(3x^2 +e^x) with the right point riemann sum on a partition of 10 subintervals
-        print("****Unit Test 03********")
-        fex1 = make_prod(make_const(3.0), make_pwr('x', 2.0))
-        fex = make_plus(fex1, make_e_expr(make_pwr('x', 1.0)))
-        print(fex)
-        err_list = riemann_approx_with_gt(fex,
-                                          make_const(-1.0),
-                                          make_const(1.0),
-                                          make_const(4.35),
-                                          make_const(10),
-                                          pp=+1)
-
-        for n, err in err_list:
-            print(n, err)
-        print("Unit Test 03: pass")
-
-    def test_04(self):
-        # ln(x )with the middle point riemann sum on a partition of 100 subintervals
-        print("****Unit Test 04********")
-
-        fex = make_ln(make_pwr('x', 1.0))
-        print(fex)
-        err = 0.0001
-        approx = riemann_approx(fex,
-                                  make_const(1.0),
-                                  make_const(2.0),
-                                  make_const(100),
-                                  pp=0)
-        print(approx.get_val())
-        assert  abs(approx.get_val() - 0.386296444432) <= err
-        print("Unit Test 04: pass")
-
-    def test_04_left_and_right_riemann(self):
-        # x^2 with 10 subintervals
-        print("****Unit Test 04********")
-
-        fex = make_pwr('x', 2.0)
-        print(fex)
-        err = 0.0001
-        approx = riemann_approx(fex,
-                                  make_const(1.0),
-                                  make_const(10.0),
-                                  make_const(10),
-                                  pp=-1)
-
-        # assert  abs(approx.get_val() - 333) <= err
-        print(approx.get_val())
-        print("Unit Test 04: pass")
-
-    def test_05(self):
-        #x^2 + 5 with the midpoint rule on a partition of 250
-        print("****Unit Test 05********")
-
-        fexpr = make_plus(make_pwr('x', 2.0),
-                                   make_const(5.0))
-        a, b, n = make_const(0.0), make_const(4.0), make_const(250)
-        approx = midpoint_rule(fexpr, a, b, n)
-        print(approx)
-        err = 0.001
-        iv = antiderivdef(fexpr, a, b)
-        print(iv)
-        assert abs(approx.get_val() - iv.get_val()) <= err
-        print("Unit Test 05: pass")
-
-    def test_06(self):
-        #x^2 + 5 with the trapezoidal rule on a partition of 350
-        print("****Unit Test 06********")
-
-        fexpr = make_plus(make_pwr('x', 2.0),
-                                   make_const(5.0))
-        a, b, n = make_const(0.0), make_const(4.0), make_const(350)
-        approx = trapezoidal_rule(fexpr, a, b, n)
-        print(approx)
-        err = 0.001
-        iv = antiderivdef(fexpr, a, b)
-        print(iv)
-        assert abs(approx.get_val() - iv.get_val()) <= err
-        print("Unit Test 06: pass")
-
-    def test_07(self):
-        # x^2 + 5 with the simpson rule on a partition of 10 subintervals
-        print("****Unit Test 07********")
-
-        fexpr = make_plus(make_pwr('x', 2.0),
-                                   make_const(5.0))
-        a, b, n = make_const(0.0), make_const(4.0), make_const(10)
-        approx = simpson_rule(fexpr, a, b, n)
-        print(approx)
-        err = 0.001
-        iv = antiderivdef(fexpr, a, b)
-        print(iv)
-        assert abs(approx.get_val() - iv.get_val()) <= err
-        print("Unit Test 07: pass")
-
-    def test_08(self):
-        # 2xe^(x^2) with the simpson rule on a partition of 100 subintervals
-        print("****Unit Test 08********")
-
-        fexpr = make_prod(make_prod(make_const(2.0),
-                                  make_pwr('x', 1.0)),
-                        make_e_expr(make_pwr('x', 2.0)))
-        a, b, n = make_const(0.0), make_const(2.0), make_const(100)
-        approx = simpson_rule(fexpr, a, b, n)
-        print(approx)
-        err = 0.001
-        assert abs(approx.get_val() - 53.5981514272) <= err
-        print("Unit Test 08: pass")
-
-    def test_09(self):
-        # (1+x^3)^0.5 with the simpson rule on a partition of 100 subintervals
-        print("****Unit Test 09********")
-
-        fex1 = make_plus(make_const(1.0),
-                        make_pwr('x', 3.0))
-        fex = make_pwr_expr(fex1, 0.5)
-        a, b, n = make_const(0.0), make_const(2.0), make_const(100)
-        approx = simpson_rule(fex, a, b, n)
-        print(approx)
-        err = 0.001
-        assert abs(approx.get_val() - 3.24124) <= err
-        print("Unit Test 09: pass")
-
-    def test_10(self):
-        print("****Unit Test 10********")
-        fex = make_prod(make_const(3.0), make_pwr('x', 2.0))
-        fex = make_plus(fex, make_e_expr(make_pwr('x', 1.0)))
-        plot_riemann_error(fex, make_const(-1.0), make_const(1.0), make_const(4.35),
-                                                    make_const(50))
-
-    def test_11(self):
-        print("****Unit Test 11********")
-        fex = make_const(4.0)
-        print(antiderivdef(fex, const(0.0), const(3.0)))
 
     if __name__ == "__main__":
         unittest.main()
